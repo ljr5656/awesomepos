9faac0bb62384eef7c90009796f566ec5b76dc5e
linjiarui1998 <ljr_1998@163.com>
2019-10-03 11:12:36 +0800

    first commmit

---
+++ awesomepos/src/components/pages/Pos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pos">
+    Hello AwesomePos demo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
