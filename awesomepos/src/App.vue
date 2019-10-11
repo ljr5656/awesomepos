5293e9a1c8e78dc4dc4718ac168e65aeac9e4b39
ljr5656 <1368298676@qq.com>
2019-10-11 12:53:38 +0800

    第二次更新

--- awesomepos/src/App.vue
+++ awesomepos/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <Pos msg="Welcome to Your Vue.js App"/>
+    <leftNav></leftNav>
+    <div class="main">
+      <Pos></Pos>
+    </div>
   </div>
 </template>
 
 <script>
-import Pos from './components/pages/Pos.vue'
+import leftNav from '@/components/common/leftNav'
+import Pos from '@/components/pages/Pos'
 
 export default {
   name: 'app',
   components: {
+    leftNav,
     Pos
   }
 }
@@ -17,11 +22,18 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height:100%;
+}
+.main{
+  float:left;
+  width:95%; 
+  background-color: #EFF2F7;
+  height:100%;
+  overflow: auto;
 }
 </style>
